7c8668054ac43a689a5fb25313a6e94f82e85c29
Boolie Kets <boolie@codingwithboolie.com>
2024-12-20 16:53:12 -0500

    Console App Tutorial
    Basic C# programming

---
+++ Tutorials/BasicCSharpConsoleApp/BasicCSharpConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int score = 100; //Declaring a variable
+        Console.WriteLine("Player's score is: " + score);
+
+        Console.WriteLine("What's your name?");
+        string firstName = Console.ReadLine() ?? "there";
+        Console.WriteLine("Hi, " + firstName + "! Welcome to C# programming!!");
+    }
+}
