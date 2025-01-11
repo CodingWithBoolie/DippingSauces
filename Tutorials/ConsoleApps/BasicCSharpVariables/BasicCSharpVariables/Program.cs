2e492cdf3dbcfedee4c9eea2681a8dd23ffce6d3
Boolie Kets <boolie@codingwithboolie.com>
2025-01-11 16:19:59 -0500

    Basic C# variables
    This is a tutorial for basic C# variables.

---
+++ Tutorials/ConsoleApps/BasicCSharpVariables/BasicCSharpVariables/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * 
+This is a multi-line comment for C#.
+You can use this one too!!
+*
+*/
+
+// This is a single-line comment. C# variable examples
+// Declare variables and assign their values
+int myNumber = 20;
+string myText = "I love coding!";
+bool isEasy = true;
+
+Console.WriteLine("Number: " + myNumber);
+Console.WriteLine("Text: " + myText);
+Console.WriteLine("Is coding easy? " + isEasy);
